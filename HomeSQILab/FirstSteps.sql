6b27508b320c1554a1501c7bc0adc7851802e7af
Fabricio <edixoncarranza@gmail.com>
2025-11-07 01:44:38 -0600

    Descripción del cambio

---
+++ HomeSQILab/FirstSteps.sql
@@ -0,0 +1,24 @@
+
+
+
+select * from academics.students
+
+
+
+
+
+--- selecionar a todos los estudiantes menores de 20 años
+select
+    id 
+    , lastname
+    , email
+    , age
+from academics.students
+where age < 19
+
+
+select * from academics.schools
+
+select * from academics.careers
+
+select * from academics.student_careers
